74adb3bdba71b0222f34035568a4b12704cbb6db
Jakub Danis <d4ny4bl3@gmail.com>
2025-01-02 20:34:53 +0100

    Section 3

---
+++ vue_counter_app_2/src/App.vue
@@ -0,0 +1,57 @@
+<!-- <script setup>
+  import { ref } from "vue";
+
+  const count = ref(0);
+
+  const addToCount = () => {
+    count.value++;
+  }
+
+  const subtractFromCount = () => {
+    count.value--;
+  }
+</script> -->
+
+<script>
+  export default {
+    data(){
+      return {
+        count: 0
+      };
+    },
+    methods: {
+      addToCount(){
+        this.count++;
+      },
+      subtractFromCount(){
+        this.count--;
+      }
+    }
+  };
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div {
+    text-align: center;
+  }
+</style>
